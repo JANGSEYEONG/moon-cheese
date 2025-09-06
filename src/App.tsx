3a77dafeaefbb061c979ea9728188f8f22ad4d5e
jangseyeong <jang.se.yeong000@gmail.com>
2025-09-06 11:55:25 +0900

    chore: add tanstack devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import router from './router';
 
 const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
