f396fdbfc8b42b0007a41ecf98844e35eeb984fe
jangseyeong <jang.se.yeong000@gmail.com>
2025-09-07 07:26:12 +0900

    feat: add useProductList and refactor query key structure

--- src/domains/product/types.ts
+++ src/domains/product/types.ts
@@ -4,3 +4,30 @@ export interface RecentProduct {
   name: string;
   price: number;
 }
+
+interface BaseProduct {
+  id: number;
+  name: string;
+  stock: number;
+  price: number;
+  description: string;
+  detailDescription: string;
+  images: string[];
+  rating: number;
+}
+
+export interface CheeseProduct extends BaseProduct {
+  category: 'CHEESE';
+}
+
+export interface CrackerProduct extends BaseProduct {
+  category: 'CRACKER';
+  isGlutenFree?: boolean;
+}
+
+export interface TeaProduct extends BaseProduct {
+  category: 'TEA';
+  isCaffeineFree?: boolean;
+}
+
+export type Product = CheeseProduct | CrackerProduct | TeaProduct;
