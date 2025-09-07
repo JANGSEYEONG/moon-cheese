b557c11850d7caec81bdea8012c93ba01c54c26f
jangseyeong <jang.se.yeong000@gmail.com>
2025-09-07 13:41:30 +0900

    feat: add EmptyCartSection

--- src/pages/ShoppingCartPage/index.tsx
+++ src/pages/ShoppingCartPage/index.tsx
@@ -1,10 +1,16 @@
+import { useCartStore } from '@/stores/cartStore';
 import CheckoutSection from './components/CheckoutSection';
 import DeliveryMethodSection from './components/DeliveryMethodSection';
 import ShoppingCartSection from './components/ShoppingCartSection';
 import { useDeliveryCharge } from './hooks/useDeliveryCharge';
+import EmptyCartSection from './components/EmptyCartSection';
 
 function ShoppingCartPage() {
+  const isCartEmpty = useCartStore(state => state.items.length <= 0);
   const { selectedDeliveryMethod, changeDeliveryMethod, deliveryRateTable } = useDeliveryCharge();
+
+  if (isCartEmpty) return <EmptyCartSection />;
+
   return (
     <>
       <ShoppingCartSection />
