cdfd2950a505ea5bad85d9719350ae728afa01a5
jangseyeong <seyeong.jang@genesisnest.com>
2025-12-11 17:56:56 +0900

    refactor: integrate GlobalProvider for enhanced state management
    - Wrapped the App component with GlobalProvider to include QueryClientProvider for React Query.
    - Updated import statements for consistency in App.tsx.

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,7 +1,13 @@
 import { EnhancedToastProvider } from '@/ui-lib/components/toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
-  return <EnhancedToastProvider>{children}</EnhancedToastProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <EnhancedToastProvider>{children}</EnhancedToastProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default GlobalProvider;
