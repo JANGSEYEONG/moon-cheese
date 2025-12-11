fdeddb5fbd961a42e49f6b5450fc0a8023ac649d
jangseyeong <seyeong.jang@genesisnest.com>
2025-12-11 18:35:52 +0900

    feat: enhance RecentPurchaseSection to display recent products with total prices
    - Integrated useSuspenseQuery to fetch recent products from the API.
    - Grouped products by ID and calculated total prices for each product.
    - Updated the component to dynamically render recent purchases with their respective images and total prices.

--- src/pages/HomePage/components/RecentPurchaseSection.tsx
+++ src/pages/HomePage/components/RecentPurchaseSection.tsx
@@ -1,8 +1,21 @@
 import { Flex, styled } from 'styled-system/jsx';
 import { Spacing, Text } from '@/ui-lib';
 import { PriceDisplay } from '@/components/PriceDisplay';
+import { getRecentProductListQueryOptions } from '@/api/getRecentProductList';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { groupBy, sumBy } from 'es-toolkit';
 
 function RecentPurchaseSection() {
+  const {
+    data: { recentProducts },
+  } = useSuspenseQuery(getRecentProductListQueryOptions());
+
+  const productMap = groupBy(recentProducts, product => product.id);
+  const productTotals = Object.values(productMap).map(products => ({
+    ...products[0],
+    totalPrice: sumBy(products, product => product.price),
+  }));
+
   return (
     <styled.section css={{ px: 5, pt: 4, pb: 8 }}>
       <Text variant="H1_Bold">최근 구매한 상품</Text>
@@ -19,51 +32,30 @@ function RecentPurchaseSection() {
         }}
         direction={'column'}
       >
-        <Flex
-          css={{
-            gap: 4,
-          }}
-        >
-          <styled.img
-            src="/moon-cheese-images/cheese-1-1.jpg"
-            alt="item"
+        {productTotals.map(product => (
+          <Flex
             css={{
-              w: '60px',
-              h: '60px',
-              objectFit: 'cover',
-              rounded: 'xl',
+              gap: 4,
             }}
-          />
-          <Flex flexDir="column" gap={1}>
-            <Text variant="B2_Medium">월레스의 오리지널 웬슬리데일</Text>
-            <Text variant="H1_Bold">
-              <PriceDisplay price={12.99} />
-            </Text>
+          >
+            <styled.img
+              src={product.thumbnail}
+              alt={product.name}
+              css={{
+                w: '60px',
+                h: '60px',
+                objectFit: 'cover',
+                rounded: 'xl',
+              }}
+            />
+            <Flex flexDir="column" gap={1}>
+              <Text variant="B2_Medium">{product.name}</Text>
+              <Text variant="H1_Bold">
+                <PriceDisplay price={product.totalPrice} />
+              </Text>
+            </Flex>
           </Flex>
-        </Flex>
-
-        <Flex
-          css={{
-            gap: 4,
-          }}
-        >
-          <styled.img
-            src="/moon-cheese-images/cheese-2-1.jpg"
-            alt="item"
-            css={{
-              w: '60px',
-              h: '60px',
-              objectFit: 'cover',
-              rounded: 'xl',
-            }}
-          />
-          <Flex flexDir="column" gap={1}>
-            <Text variant="B2_Medium">그랜드 데이 아웃 체다</Text>
-            <Text variant="H1_Bold">
-              <PriceDisplay price={14.87} />
-            </Text>
-          </Flex>
-        </Flex>
+        ))}
       </Flex>
     </styled.section>
   );
