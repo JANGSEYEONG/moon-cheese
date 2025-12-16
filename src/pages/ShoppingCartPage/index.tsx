52e967210f471b01be8d09afb11db87e6a73b544
jangseyeong <jang.se.yeong000@gmail.com>
2025-12-16 18:07:29 +0900

    feat: add EmptyCartSection to handle empty cart state in ShoppingCartPage
    - Integrated useCartStore to check cart contents and conditionally render EmptyCartSection when the cart is empty.
    - Updated EmptyCartSection to include a link for users to navigate back to the shopping page.

--- src/pages/ShoppingCartPage/index.tsx
+++ src/pages/ShoppingCartPage/index.tsx
@@ -1,15 +1,23 @@
-import CheckoutSection from "./components/CheckoutSection";
-import DeliveryMethodSection from "./components/DeliveryMethodSection";
-import ShoppingCartSection from "./components/ShoppingCartSection";
+import { useCartStore } from '@/stores/useCartStore';
+import CheckoutSection from './components/CheckoutSection';
+import DeliveryMethodSection from './components/DeliveryMethodSection';
+import ShoppingCartSection from './components/ShoppingCartSection';
+import EmptyCartSection from './components/EmptyCartSection';
 
 function ShoppingCartPage() {
-	return (
-		<>
-			<ShoppingCartSection />
-			<DeliveryMethodSection />
-			<CheckoutSection />
-		</>
-	);
+  const cart = useCartStore(state => state.cart);
+
+  if (cart.length === 0) {
+    return <EmptyCartSection />;
+  }
+
+  return (
+    <>
+      <ShoppingCartSection />
+      <DeliveryMethodSection />
+      <CheckoutSection />
+    </>
+  );
 }
 
 export default ShoppingCartPage;
